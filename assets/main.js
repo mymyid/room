512baa5ff5a6f3cbc2a114895969a0d8575516c3
アウリア <abdul.haq.aulia@gmail.com>
2024-06-06 16:57:40 +0700

    feat: tambah main page

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+console.log("hello cah...");
+
+const localVideo = document.getElementById("localVideo");
+const peerCamera = document.getElementById("peerCamera")
+// Get user media (camera)
+const stream = await navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+});
+localVideo.srcObject = stream;
+
+// TODO : Ubah value dengan media stream dari peer nantinya
+peerCamera.srcObject = stream
