4a504b29f0e477eec30efd78a2aeef203922b6a5
Moh. Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2024-06-11 07:46:59 +0700

    feat(api): tambah integrasi create room

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -1,5 +1,6 @@
 import { provider, auth } from "../firebase.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+const URL = "http://127.0.0.1:54889";
 
 document.getElementById("sign-out").addEventListener("click", function () {
   signOut(auth)
@@ -7,15 +8,37 @@ document.getElementById("sign-out").addEventListener("click", function () {
       window.location.href = "index.html";
     })
     .catch((error) => {
-      alert(error)
+      alert(error);
     });
 });
 
+document.getElementById("simpan-button").addEventListener("click", createRom);
+
+async function createRom() {
+  const judul = document.getElementById("judul");
+  if (!judul) {
+    alert("Silahkan isi judul meeting terlebih dahulu")
+    return
+  }
+  
+  const payload = { judul: judul.value }; // Payload containing the title
+  console.log(payload)
+  const response = await fetch(`${URL}/api/create-room`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+  const data = await response.json();
+  console.log(data);
+  document.getElementById("roomID").innerText = `Room ID: ${data.roomID}`;
+}
 
 console.log("hello cah on home... 👋");
 
 const localVideo = document.getElementById("localVideo");
-const peerCamera = document.getElementById("peerCamera")
+const peerCamera = document.getElementById("peerCamera");
 // Get user media (camera)
 const stream = await navigator.mediaDevices.getUserMedia({
   video: true,
