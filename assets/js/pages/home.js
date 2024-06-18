c558272476a930e708a2db2dabcd9064caf1ba6e
アウリア <abdul.haq.aulia@gmail.com>
2024-06-18 14:45:29 +0700

    fix(comunicaitno): handle proses pertukaran dengan long polling

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -1,7 +1,10 @@
 import { provider, auth } from "../firebase.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-const URL = "http://127.0.0.1:54889";
 
+const emptySpace = document.getElementById("empty-info");
+const roomList = document.getElementById("room-list");
+const URL = "http://127.0.0.1:8080";
+var isloading = false
 document.getElementById("sign-out").addEventListener("click", function () {
   signOut(auth)
     .then(() => {
@@ -14,25 +17,86 @@ document.getElementById("sign-out").addEventListener("click", function () {
 
 document.getElementById("simpan-button").addEventListener("click", createRom);
 
-async function createRom() {
-  const judul = document.getElementById("judul");
-  if (!judul) {
-    alert("Silahkan isi judul meeting terlebih dahulu")
-    return
+getInitialRoom();
+
+async function getInitialRoom() {
+  let data = await getRooms();
+  await generateList(data);
+}
+
+async function generateList(rooms) {
+  const dataRooms = Object.keys(rooms);
+  if (dataRooms.length > 0) {
+    emptySpace.classList.add("hidden");
+    roomList.classList.remove('hidden');
+    console.log(dataRooms.length)
+    roomList.innerHTML = ""
+    for (let index = 0; index < dataRooms.length; index++) {
+      const element = rooms[dataRooms[index]];
+      console.log(element)
+      const li = document.createElement("li");
+      li.className = "bg-white rounded-xl p-3 mb-3";
+      const uid = localStorage.getItem("_userid")
+      li.innerHTML = `
+        <a href="room.html?id=${element.ID}&uid=${uid}" class="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mx-3 text-green-600">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+            </svg>
+            <div class="flex flex-row w-full">
+                <p class="font-bold flex w-full">${element.Title}.</p>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+            </div>
+        </a>
+      `;
+      roomList.append(li);
+    }
+  } else {
+    emptySpace.classList.remove("hidden");
+    roomList.classList.add('hidden');
   }
-  
-  const payload = { judul: judul.value }; // Payload containing the title
-  console.log(payload)
-  const response = await fetch(`${URL}/api/create-room`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  });
-  const data = await response.json();
-  console.log(data);
-  document.getElementById("roomID").innerText = `Room ID: ${data.roomID}`;
+
+}
+async function createRom() {
+  if (isloading) return
+  isloading = true
+  const judul = document.getElementById("judul");
+
+  if (!judul.value) {
+    isloading = false
+    alert("Silahkan isi judul meeting terlebih dahulu");
+    return;
+  }
+
+  try {
+    const payload = { judul: judul.value }; // Payload containing the title
+    
+    const response = await fetch(`${URL}/api/create-room`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    const data = await response.json();
+    judul.value = "";
+    isloading = false
+    getInitialRoom()
+  } catch (error) {
+    isloading = false
+    alert(error);
+  }
+}
+
+async function getRooms() {
+  try {
+    const response = await fetch(`${URL}/api/rooms`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    alert(error);
+  }
 }
 
 console.log("hello cah on home... 👋");
@@ -42,6 +106,6 @@ const peerCamera = document.getElementById("peerCamera");
 // Get user media (camera)
 const stream = await navigator.mediaDevices.getUserMedia({
   video: true,
-  audio: true,
+  audio: false,
 });
 localVideo.srcObject = stream;
--- assets/js/pages/room.js
+++ assets/js/pages/room.js
@@ -2,19 +2,192 @@ import { provider, auth } from "../firebase.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 console.log("hello cah on room... 👋");
-
+const URL = "http://127.0.0.1:8080";
 const localVideo = document.getElementById("localVideo");
 const peerCamera = document.getElementById("peerCamera");
-// Get user media (camera)
-try {
-  const stream = await navigator.mediaDevices.getUserMedia({
+var peerConnection
+initVideoCall();
+
+async function initVideoCall() {
+  // Example usage: Get the 'id' query parameter
+  const roomId = getQueryParameter("id");
+  console.log("Room ID:", roomId);
+
+  // Get user media (camera)
+  const localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true,
+    audio: false,
   });
-  localVideo.srcObject = stream;
-  peerCamera.srcObject = stream;
-} catch (error) {
-  console.error("Error accessing media devices:", error);
+  localVideo.srcObject = localStream;
+
+  // Create a new RTCPeerConnection
+  peerConnection = new RTCPeerConnection({
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  });
+
+  // Add tracks from the local localStream to the peer connection
+  localStream.getTracks().forEach((track) => peerConnection.addTrack(track, localStream));
+
+  // Handle ICE candidates from the peer connection
+  peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      // fetch(`${URL}/api/room/${roomId}/join`, {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify({ type: "candidate", data: event.candidate }),
+      // });
+    }
+  };
+
+  // Handle incoming remote tracks
+  peerConnection.ontrack = (event) => {
+    console.log(event)
+    if (peerCamera.srcObject !== event.streams[0]) {
+      console.log("onTrack Stream >>", event.streams);
+      console.log("onTrack First Stream Data >> ", event.streams[0]);
+      // peerCamera.srcObject = event.streams[0];
+      console.log("Received remote stream");
+    }
+  };
+
+  // Handle errors
+  peerConnection.onerror = (error) => {
+    console.error("PeerConnection error:", error);
+  };
+
+  peerConnection.onconnectionstatechange = (event) => {
+    console.log("Connection state changed:", peerConnection.connectionState);
+  };
+
+  // Create an SDP offer
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  const uid = localStorage.getItem("_userid");
+  // Send the offer to the server and get the answer
+  const response = await fetch(`${URL}/api/room/${roomId}/join/${uid}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ type: "offer", data: offer }),
+  });
+
+  if (!response.ok) {
+    console.error("Failed to get answer from server:", response.statusText);
+    if (response.status == 404) {
+      alert(`Room ${response.statusText}, Silahkan buat room baru`);
+      window.location.href = "home.html";
+    }
+    return;
+  }
+
+  const data = await response.json();
+  console.log("data >> ", data);
+  console.log("Success Join Room, periodicaly get offer then send onswer");
+
+  var interval = setInterval(function () {
+    // Get Offer
+    
+    getOffer().then((od) => {
+      console.log("offer Data >> ", od);
+      if (od) clearInterval(interval);
+    });
+  }, 2000);
+  // const answer = new RTCSessionDescription(data.data);
+
+  // // Set the remote description with the server's answer
+  // await peerConnection.setRemoteDescription(answer);
+
+  // console.log("WebRTC connection established successfully!");
+}
+
+async function getOffer() {
+  let offer = false;
+  const roomId = getQueryParameter("id");
+  const now = new Date();
+  console.log("Getting Offer >> ", now);
+  const uid = localStorage.getItem("_userid");
+  // Send the offer to the server and get the answer
+  const response = await fetch(`${URL}/api/room/${roomId}/data/${uid}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (!response.ok) {
+    console.error("Failed to get answer from server:", response.statusText);
+    return;
+  }
+
+  const res = await response.json();
+  console.log("Offer >> ", res);
+  const offersData = res.room.Offers;
+  if (res.room.Offers.length > 0) {
+    // Get Offer for me
+    let offerToMe = offersData.filter((item) => item.Uid != uid);
+    console.log("Offer to me >> ", offerToMe);
+    if (offerToMe.length > 0) {
+      const answer = new RTCSessionDescription(offerToMe[0].Offer);
+      console.log("Sending Answer >> ", answer);
+      offer = true;
+      sendAnswer(answer);
+    }
+  }
+  return offer;
+}
+
+async function sendAnswer(answer) {
+  const now = new Date();
+  console.log("Send Answer >> ", now);
+  const roomId = getQueryParameter("id");
+  const uid = localStorage.getItem("_userid");
+  const response = await fetch(`${URL}/api/room/${roomId}/join/${uid}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ type: "answer", data: answer }),
+  });
+
+  if (!response.ok) {
+    console.error("Failed to send answer to server:", response.statusText);
+    return;
+  }
+
+  var intervalAnswer = setInterval(function () {
+    let answerData = getAnswer();
+    if (answerData) clearInterval(intervalAnswer);
+  }, 2000);
+}
+
+async function getAnswer() {
+  const roomId = getQueryParameter("id");
+  const now = new Date();
+  console.log("Getting Offer >> ", now);
+  const uid = localStorage.getItem("_userid");
+  // Send the offer to the server and get the answer
+  const response = await fetch(`${URL}/api/room/${roomId}/data/${uid}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (!response.ok) {
+    console.error("Failed to get answer from server:", response.statusText);
+    return;
+  }
+
+  const res = await response.json();
+  const answersData = res.room.Answer;
+  console.log("data answer >>", answersData);
+  if (res.room.Answer.length > 0) {
+    // Get Offer for me
+    const answerData = res.room.Answer;
+    let answerForMe = answerData.filter((item) => item.Uid != uid);
+    if (answerForMe.length > 0) {
+      // TODO
+      // Set rtc client
+      const remoteAnswer = answerForMe[0];
+      console.log("SUCCESS GETTING DATA, LET SET STRAEM DATA", answerForMe);
+      // Set the remote description with the server's answer
+      await peerConnection.setRemoteDescription(remoteAnswer.Answer);
+      document.getElementById('status-connection').innerHTML = "Connected"
+    }
+  }
 }
 
 document.getElementById("sign-out").addEventListener("click", function () {
@@ -26,3 +199,8 @@ document.getElementById("sign-out").addEventListener("click", function () {
       alert(error);
     });
 });
+
+function getQueryParameter(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
